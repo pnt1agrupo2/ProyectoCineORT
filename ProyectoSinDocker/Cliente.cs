8a197803cba52ee3271030ffa2deb659a6663149
Javier Iván Moreno <Rannmoreno@gmail.com>
2022-10-11 20:14:32 -0300

    Carpetas

---
+++ ProyectoSinDocker/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoCine
+{
+    class Cliente
+    {
+        private int edad;
+        private String nombre;
+        private String apellido;
+    }
+}
---
+++ ProyectoSinDocker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoCine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
