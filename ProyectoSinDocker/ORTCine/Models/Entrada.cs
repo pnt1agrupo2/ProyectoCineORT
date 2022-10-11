14a4fba6779e50715d6ee9ff8068d3c0df89437e
Javier Iván Moreno <Rannmoreno@gmail.com>
2022-10-11 20:22:46 -0300

    Creacion de Soluciones
    Una con docker para correr mas rapido
    Y otra sin docker para que sea mas portable
    Ambos casi clones

---
+++ ProyectoSinDocker/ORTCine/Models/Entrada.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoCine
+{
+    class Entrada
+    {
+        private double valor;
+        private int numeroButaca;
+        private String sala;
+
+    }
+}
---
+++ ProyectoSinDocker/ORTCine/Models/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoCine
+{
+    class Pelicula
+    {
+        private String nombre;
+        private String genero;
+        private Boolean estaDoblada;
+        private Boolean esAtp;
+    }
+}
